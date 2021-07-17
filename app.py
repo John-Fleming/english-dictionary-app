608a8354df83933ecf06fd56cb91d70e5d1131d8
John Fleming <johndeclanfleming@gmail.com>
2021-07-17 18:29:34 -0500

    formatting results with multiple defintions

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def find_closest_word(word):
   matched_word = get_close_matches(word, definitions.keys(), cutoff=0.8)
 
   if len(matched_word):
-    user_feedback = input("Did you mean '%s' instead? Enter y or n" % matched_word[0])
+    user_feedback = input("Did you mean '%s' instead? Enter y or n: " % matched_word[0])
     return definitions[matched_word[0]] if user_feedback.lower() == 'y' else None
 
 def look_up_defintion(word):
@@ -21,4 +21,10 @@ def look_up_defintion(word):
   
 word_to_search = input('Enter a word to look up: ')
 
-print(look_up_defintion(word_to_search))
+result = look_up_defintion(word_to_search)
+
+if type(result) == list:
+  for definition in result:
+    print(definition)
+else:
+  print(result)
